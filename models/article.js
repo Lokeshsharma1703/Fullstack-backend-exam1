754a1bd55376c5ae9387a08f7df6bcb89ffb8579
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-03-03 16:20:51 +0530

    initial commit

---
+++ models/article.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    description: {
+        type: String
+    },
+    markdown: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    slug: {
+        type: String,
+    },
+    sanitizedHtml: {
+        type: String,
+    }
+})
+
+
+module.exports = mongoose.model('Article', articleSchema)
